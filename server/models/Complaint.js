2bc6ddc47ab933d263209a1058377b112d2ab27f
ruhi <shaikruhi2233@gmail.com>
2026-07-01 12:30:57 +0530

    Added complaint location

--- server/models/Complaint.js
+++ server/models/Complaint.js
@@ -5,25 +5,36 @@ const complaintSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
+      trim: true,
     },
 
     email: {
       type: String,
       required: true,
+      trim: true,
     },
 
     subject: {
       type: String,
       required: true,
+      trim: true,
     },
 
     complaint: {
       type: String,
       required: true,
+      trim: true,
+    },
+
+    location: {
+      type: String,
+      required: true,
+      trim: true,
     },
 
     status: {
       type: String,
+      enum: ["Pending", "Resolved"],
       default: "Pending",
     },
   },
